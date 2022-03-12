a7ed64aeee4d3ee7cd2f784c24db669bcc56efee
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-03-12 16:29:34 -0700

    commit tree 5fe1b8beb2d23e4a7bcb574b0e6ce703c62078f1

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,32 @@
 // Code DigitalClicker Component Here
+
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component {
+    constructor() {
+        super()
+        this.state = {timesClicked: 0}
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return {timesClicked: previousState.timesClicked + 1}
+        })
+
+        // all three of these seem to work...
+
+        // let newClick = this.state.timesClicked + 1
+        // this.setState({timesClicked: newClick})
+
+        // this.setState({timesClicked: this.state.timesClicked + 1})
+    }
+
+    render() {
+        return (
+            <div>
+                {/* {console.log(this.state)} */}
+                <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,73 @@
 // Code YouTubeDebugger Component Here
+
+import React, {Component} from 'react'
+
+export default class YouTubeDebugger extends Component {
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    handleBitrateButton = () => {
+        this.setState(previousState => {
+            return {
+                settings: {
+                    ...previousState.settings,
+                    bitrate: 12
+                }
+            }
+        })
+
+        // these both seem to work...
+
+        // this.setState({
+        //     settings: {
+        //         ...this.state.settings,
+        //         bitrate: 12
+        //     }
+        // })
+    }
+
+    handleResButton = () => {
+        this.setState(previousState => {
+            return {
+                settings: {
+                    ...previousState.settings,
+                    video: {
+                        resolution: '720p'
+                    }
+                }
+            }
+        })
+
+        // these both seem to work...
+
+        // this.setState({
+        //     settings: {
+        //         ...this.state.settings,
+        //         video: {
+        //             resolution: '720p'
+        //         }
+        //     }
+        // })
+    }
+    
+    render() {
+        return (
+            <div>
+                {/* {console.log(this.state)} */}
+                <button className='bitrate' onClick={this.handleBitrateButton}>{this.state.settings.bitrate}</button>
+                <button className='resolution' onClick={this.handleResButton}>{this.state.settings.video.resolution}</button>
+            </div>
+        )
+    }
+}
